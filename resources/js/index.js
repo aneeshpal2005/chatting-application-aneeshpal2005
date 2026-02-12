d80c7289aa5c956b01d7cd9140daaf0a4aa8e4e7
Aneesh Palande <45935068+aneeshpal2005@users.noreply.github.com>
2026-02-12 12:31:24 -0500

    added updateMessages

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,3 +3,79 @@ const myMessage = document.getElementById(`my-message`);
 const sendButton = document.getElementById(`send-button`);
 const chatBox = document.getElementById(`chat`);
 
+function formatMessage(message, myNameInput) {
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+
+  if (myNameInput === message.sender) {
+    return `
+      <div class="mine messages">
+        <div class="message">
+          ${message.text}
+        </div>
+        <div class="sender-info">
+          ${formattedTime}
+        </div>
+      </div>
+    `;
+  } else {
+    return `
+      <div class="yours messages">
+        <div class="message">
+          ${message.text}
+        </div>
+        <div class="sender-info">
+          ${message.sender} ${formattedTime}
+        </div>
+      </div>
+    `;
+  }
+}
+
+function fetchMessages() {
+  return [
+    {
+      id: 1,
+      text: `This is my message`,
+      sender: `Yahya Gilany`,
+      timestamp: 1537410673072
+    },
+    {
+      id: 2,
+      text: `This is another message`,
+      sender: `Yahya Gilany`,
+      timestamp: 1537410673072
+    },
+    {
+      id: 3,
+      text: `This is a message from someone else`,
+      sender: `Someone Else`,
+      timestamp: 1537410673072
+    }
+  ];
+}
+
+function updateMessages() {
+  const messages = fetchMessages();
+  let formattedMessages = ``;
+
+  messages.forEach(message => {
+    formattedMessages += formatMessage(message, nameInput.value);
+  });
+  chatBox.innerHTML = formattedMessages;
+}
+
+updateMessages();
+
+sendButton.addEventListener(`click`, () => {
+  const newMessage = {
+    id: Date.now(),
+    text: myMessage.value,
+    sender: nameInput.value,
+    timestamp: Date.now()
+  };
+
+  const formattedMessage = formatMessage(newMessage, nameInput.value);
+  chatBox.innerHTML += formattedMessage;
+  myMessage.value = ``;
+});
